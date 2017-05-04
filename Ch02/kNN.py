2f1066ccd3c91e135ac78e27c8538465f606ca31
ys <ys.ahn@outlook.com>
2017-05-04 14:18:02 +0900

    2.2.1 Ready to analysis

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -23,7 +23,29 @@ def classify0(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
+def file2matrix(filename):
+    love_dictionary={'largeDoses': 3, 'smallDoses': 2, 'didntLike': 1}
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)            #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        if(listFromLine[-1].isdigit()):
+            classLabelVector.append(int(listFromLine[-1]))
+        else:
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
 if __name__ == '__main__':
-    group, labels = createDataSet()
-    result = classify0([0, 0], group, labels, 3)
-    print(result)
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    print('datingDataMat')
+    print(datingDataMat)
+    print()
+    print('datingLabels')
+    print(datingLabels)
