a2ee5c88697f7c326dd81874fae9304f7cbb5879
YoungSeon.Ahn <ys.ahn@outlook.com>
2017-05-04 23:16:27 +0900

    2.3.2 Test

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -1,6 +1,7 @@
 from numpy import *
 import operator
 import matplotlib.pyplot as plt
+from os import listdir
 
 
 def createDataSet():
@@ -104,7 +105,32 @@ def img2vector(filename):
     return returnVect
 
 
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')        #iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+
+
 if __name__ == '__main__':
-    testVector = img2vector('testDigits/0_13.txt')
-    print(testVector[0, 0:31])
-    print(testVector[0, 32:63])
+    handwritingClassTest()
